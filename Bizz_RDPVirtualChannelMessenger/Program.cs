1c6f955263a011d2605aa991e8faefb99d3852ad
Luke Schofield <luke.schofield@cam-systems.co.uk>
2019-06-14 11:21:19 +0100

    Add project files.

---
+++ Bizz_RDPVirtualChannelMessenger/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using Bizz_RDPVirtualChannelMessenger;
+
+namespace Bizz_RDPVirtualChannelMessengerServer
+{
+    
+    class Program
+    {
+        static string channelName = "BizzVC";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please press enter to begin...");
+            Console.ReadLine();
+            // Open a virtual channel
+            VirtualChannel.OpenVirtualChannel(channelName);
+            Console.WriteLine("Connected!" + Environment.NewLine);
+
+            DisplayChoice();
+
+        }
+        static void DisplayChoice()
+        {
+            Console.WriteLine($"Please select what you would like to do:{Environment.NewLine}" +
+                                $"1     : Send String {Environment.NewLine}" +
+                                $"2     : Read String  {Environment.NewLine}" + 
+                                $"Space : Exit");
+            var choice = Console.ReadKey().Key;
+            switch (choice)
+            {
+                case ConsoleKey.D1:
+                    {
+                        Console.WriteLine($"Please enter text to send: {Environment.NewLine}");
+                        var ret = VirtualChannel.WriteChannel(Console.ReadLine());
+                        Console.WriteLine($"Write to channel returned: {ret}.");
+                        Console.WriteLine("Press enter to continue...");
+                        Console.ReadLine();
+                        DisplayChoice();
+                        break;
+                    }
+                case ConsoleKey.D2:
+                    {
+                        Console.WriteLine(VirtualChannel.ReadChannel());
+                        DisplayChoice();
+                        break;
+                    }
+                case ConsoleKey.Spacebar:
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine($"Invalid input. {Environment.NewLine}{Environment.NewLine}{Environment.NewLine}");
+                        DisplayChoice();
+                        break;
+                    }
+            }
+        }
+    }
+
+    public static class VirtualChannel
+    {
+        static IntPtr mHandle = IntPtr.Zero;
+        public static void OpenVirtualChannel(string channelName)
+        {
+            mHandle = WtsApi32.WTSVirtualChannelOpen(IntPtr.Zero, -1, channelName);
+        }
+
+        public static string ReadChannel()
+        {
+            return "Not yet implemented";
+        }
+        public static bool WriteChannel(string message)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            int bytesWritten = 0;
+            bool returnValue = WtsApi32.WTSVirtualChannelWrite(mHandle, data, data.Length, ref bytesWritten);
+            if(bytesWritten == data.Length)
+            {
+                Console.WriteLine($"Written string of {message} as a total of {data.Length.ToString()} bytes.");
+            }
+            return returnValue;
+        }
+    }
+}
---
+++ Bizz_RDPVirtualChannelMessengerClient/VirtualChannelClient.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Bizz_RDPVirtualChannelMessengerClient.WtsApi32;
+
+namespace Bizz_RDPVirtualChannelMessengerClient
+{
+    class VirtualChannelClient
+    {
+        static IntPtr Channel;
+        static ChannelEntryPoints EntryPoints;
+        static int OpenChannel = 0;
+        const string channelName = "BizzVC";
+
+        static ChannelInitEventDelegate channelInitEventDelegate = new ChannelInitEventDelegate(VirtualChannelInitEventProc);
+        static ChannelOpenEventDelegate channelOpenEventDelegate = new ChannelOpenEventDelegate(VirtualChannelOpenEvent);
+
+        // https://docs.microsoft.com/en-us/windows/desktop/termserv/virtual-channel-client-dll 
+        [DllExport]
+        public static bool VirtualChannelEntry(ref ChannelEntryPoints entry)
+        {
+            ChannelDef[] channeldefinition = new ChannelDef[1];
+            channeldefinition[0] = new ChannelDef();
+            EntryPoints = entry;
+
+            channeldefinition[0].name = channelName;
+            ChannelReturnCodes ret = EntryPoints.VirtualChannelInit(ref Channel, channeldefinition, 1, 1, channelInitEventDelegate);
+
+            if (ret != ChannelReturnCodes.Ok)
+            {
+                // Failed call of VirtualChannelEntry
+                string[] output = { $"Failed VirtualChannelEntry - {ret.ToString()} : " + DateTime.Now.ToUniversalTime() };
+                System.IO.File.WriteAllLines(@"C:\Users\luke.schofield\Desktop\Output.txt", output);
+            }
+            else
+            {
+                //Success call of VirtualChannelEntry
+                string[] output = { $"Success VirtualChannelEntry - {ret.ToString()} : " + DateTime.Now.ToUniversalTime() };
+                System.IO.File.WriteAllLines(@"C:\Users\luke.schofield\Desktop\Output.txt", output);
+            }
+            return true;
+        }
+
+        private static void VirtualChannelInitEventProc(IntPtr initHandle, ChannelEvents Event, byte[] data, int dataLength)
+        {
+            string[] output = { };
+            switch (Event)
+            {
+                case ChannelEvents.Initialized:
+                    output = new string[] { $"Channel initialised. : " + DateTime.Now.ToUniversalTime() };
+                    System.IO.File.WriteAllLines(@"C:\Users\luke.schofield\Desktop\Output.txt", output);
+                    break;
+                case ChannelEvents.Connected:
+                    output = new string [] { $"Channel connected. Calling to channel open event delegate : " + DateTime.Now.ToUniversalTime() };
+                    System.IO.File.WriteAllLines(@"C:\Users\luke.schofield\Desktop\Output.txt", output);
+                    ChannelReturnCodes ret = EntryPoints.VirtualChannelOpen(initHandle, ref OpenChannel, channelName, channelOpenEventDelegate);
+                    break;
+                case ChannelEvents.V1Connected:
+                    output = new string[] { $"v1 connected. : " + DateTime.Now.ToUniversalTime() };
+                    System.IO.File.WriteAllLines(@"C:\Users\luke.schofield\Desktop\Output.txt", output);
+                    break;
+                case ChannelEvents.Disconnected:
+                    output = new string[] { $"Disconnected. : " + DateTime.Now.ToUniversalTime() };
+                    System.IO.File.WriteAllLines(@"C:\Users\luke.schofield\Desktop\Output.txt", output);
+                    break;
+                case ChannelEvents.Terminated:
+                    output = new string[] { $"Terminated. : " + DateTime.Now.ToUniversalTime() };
+                    System.IO.File.WriteAllLines(@"C:\Users\luke.schofield\Desktop\Output.txt", output);
+                    break;
+            }
+        }
+        private static void VirtualChannelOpenEvent(int openHandle, ChannelEvents Event, byte[] data, int dataLength, uint totalLength, ChannelFlags dataFlags)
+        {
+            string[] output = new string[] { $"Channel Open Event Fired. This is good news!!! : " + DateTime.Now.ToUniversalTime(), Event.ToString(), dataFlags.ToString(),
+                                            $"Data received through Virtual Channel : {Encoding.ASCII.GetString(data)}" };
+            System.IO.File.WriteAllLines(@"C:\Users\luke.schofield\Desktop\Output.txt", output);
+        }
+    }
+
+}
